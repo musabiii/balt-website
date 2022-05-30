ff602b0dc2ab01252f0fc237f8ad8db2501552b5
Musabii Mambetov <musabi@live.com>
2022-05-30 23:44:49 +0300

    карусель

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import "./style.scss";
-// import './assets/vbdlogo.png';
+
+const inn = document.querySelector('.box-inn');
+const next = document.querySelector('.right-control');
+const prev = document.querySelector('.left-control');
+
+//closure for save offset value
+const offsetControl =(offset=0)=>(len=1170)=>{
+        offset +=len;
+        if (offset>1170*2) offset = 0;
+        if (offset<0) offset = 1170*2;
+        inn.style.transform = `translateX(-${offset}px)`;
+}
+
+const letOffset = offsetControl();
+
+setInterval(letOffset,5000); //auto shift
+
+next.addEventListener('click',()=>{
+    letOffset();
+})
+
+prev.addEventListener('click',()=>{
+    letOffset(-1170);
+})
